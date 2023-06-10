484cf9a696830f95fc3a0ca8b7b4d8e6939956f2
inifares23lab <gitms@pm.me>
2023-06-10 15:07:37 +0100

    add unit tests

---
+++ errors_test.go
@@ -0,0 +1,78 @@
+package errors
+
+import (
+	"errors"
+	"strings"
+	"testing"
+)
+
+// TestWrap tests the Wrap function.
+//
+// It provides tests for various scenarios where Wrap is provided with different
+// input parameters. The function tests if Wrap behaves as expected and if the
+// error message contains the expected output. The function doesn't return
+// anything.
+func TestWrap(t *testing.T) {
+	t.Run("Test when description and err are both nil", func(t *testing.T) {
+		err := Wrap("", nil)
+		if err != nil {
+			t.Errorf("Expected nil error but got %v", err)
+		}
+	})
+
+	t.Run("Test when only description is provided", func(t *testing.T) {
+		description := "test error"
+		err := Wrap(description, nil)
+		if err == nil {
+			t.Errorf("Expected non-nil error but got nil")
+		}
+		if !strings.Contains(err.Error(), description) {
+			t.Errorf("Expected error to contain %s but got %s", description, err.Error())
+		}
+		at := "at "
+		if !strings.Contains(err.Error(), at) {
+			t.Errorf("Expected error to contain %s but got %s", at, err.Error())
+		}
+	})
+
+	t.Run("Test when only err is provided", func(t *testing.T) {
+		innerErr := errors.New("inner error")
+		err := Wrap("", innerErr)
+		if err == nil {
+			t.Errorf("Expected non-nil error but got nil")
+		}
+		if !errors.Is(err, innerErr) {
+			t.Errorf("Expected error to contain wrapped %v but got %v", innerErr, err)
+		}
+		if !strings.Contains(err.Error(), _NO_DESCRIPTION) {
+			t.Errorf("Expected error to contain %s but got %s", _NO_DESCRIPTION, err.Error())
+		}
+		causedBy := "caused by:"
+		if !strings.Contains(err.Error(), causedBy) {
+			t.Errorf("Expected error to contain %s but got %s", causedBy, err.Error())
+		}
+	})
+
+	t.Run("Test when description and err are both provided", func(t *testing.T) {
+		description := "test error"
+		innerErr := errors.New("inner error")
+		err := Wrap(description, innerErr)
+		if err == nil {
+			t.Errorf("Expected non-nil error but got nil")
+		}
+		if !errors.Is(err, innerErr) {
+			t.Errorf("Expected error to contain wrapped %v but got %v", innerErr, err)
+		}
+		if !strings.Contains(err.Error(), description) {
+			t.Errorf("Expected error to contain %s but got %s", description, err.Error())
+		}
+		causedBy := "caused by:"
+		if !strings.Contains(err.Error(), causedBy) {
+			t.Errorf("Expected error to contain %s but got %s", causedBy, err.Error())
+		}
+		at := "at "
+		if strings.Contains(err.Error(), at) {
+			t.Errorf("Expected error to not contain %s but got %s", at, err.Error())
+		}
+	})
+}
