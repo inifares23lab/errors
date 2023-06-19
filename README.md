71e618326fcc7e23e055d9fb87561e0ce9e89a3b
inifares23lab <gitms@pm.me>
2023-06-19 08:50:32 +0100

    simplified

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # errors
 
-It provides same exact functionality as standard "errors" plus 2 functions:
+It provides same exact functionality as standard "errors" plus 3 exported functions:
 
-- Wrap(description string, err error) error:
-It wraps the errors with the standard fmt.Errorf("... %w", ..) in an opinionated way.
-It also locates the error if it is the first in the chain or in the absence of a descriptive message.
-- Locate(err error) error:
-Locates the errors more flexibly.
+- ```Wrap(description string, err error) error```:
+It wraps the errors with the standard fmt.Errorf("... %w", ..) adding a default description if not provided.
+- ```WrapLocate(description string, err error) error```:
+Same as ```Wrap``` but it locates the position where the error is generated.
+- ```NewLocate(description string) error```:
+Same as ```New``` but it locates the position where the error is generated.
+- In addition a default description is enforced for the standard ```errors.New```
 
-The assumptions are that I want to know what and when it happened but without adding much overhead hence the conditional location functionality and the "sligtly enforced" description.
-
-- The Join functionality is also unchanged but the output is more readable with the formatting in use here
+I don't want a fancy package. It can be a drop-in replacement for the standard errors package.
--- custom.go
+++ custom.go
@@ -1,8 +1,9 @@
 package errors
 
 import (
-	"errors"
 	"fmt"
+	"runtime"
+	"runtime/debug"
 )
 
 const (
@@ -11,8 +12,6 @@ const (
 )
 
 // Wrap formats an error message with an optional underlying error wrapped as its cause.
-// It locates the error only if it is the first in the chain or if a descripion is missing
-// to improve the debugging experience without adding too much overhead.
 // Args:
 //
 //	description (string): The description of the error.
@@ -20,17 +19,19 @@ const (
 //
 // Returns:
 //
-//	error: The wrapped error with or without the location of the caller.
+//	error: The wrapped errors.
 func Wrap(description string, cause error) error {
 	if description == "" {
 		description = _NO_DESCRIPTION
 	}
 	if cause == nil {
-		return errors.New(description)
+		return fmt.Errorf("%s", description)
 	}
 	return fmt.Errorf("%s%s%w", description, _CAUSED_BY, cause)
 }
 
+// WrapLocate formats an error message with an optional underlying error wrapped as its cause.
+// It also locates the error in order to improve the debugging experience.
 func WrapLocate(description string, cause error) error {
 	if description == "" {
 		description = _NO_DESCRIPTION
@@ -41,6 +42,31 @@ func WrapLocate(description string, cause error) error {
 	return fmt.Errorf("%s%s%w", locateAt(description, 2), _CAUSED_BY, cause)
 }
 
+// NewLocate creates a new error with location from a given string.
+//
+// str: the string to create the location from. If empty, a default string will be used.
+// error: the created error
 func NewLocate(str string) error {
+	if str == "" {
+		str = _NO_DESCRIPTION
+	}
 	return locateAt(str, 2)
+}
+
+// locateAt is used to locate the caller that generates this error.
+// Args:
+//
+//	str (string): The string of the error message.
+//	skip (int): Defaults to 2 at the moment, but provided for future extensions.
+//
+// Returns:
+//
+//	error: The error with the location of the caller.
+func locateAt(str string, skip int) error {
+	if _, file, line, ok := runtime.Caller(skip); ok {
+		return fmt.Errorf("%s\n\tat \"%s:%d\"", str, file, line)
+	}
+	// this should never happen but if it does it adds the goroutine stacktrace with a little extra overhead
+	return fmt.Errorf("%s\n\tat \"could not locate the error, getting stacktrace:\n(%s)\"",
+		str, debug.Stack())
 }
